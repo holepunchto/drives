9dfedaa7b5fe9f35deb7f87132e4cd6a6fb280cb
Lucas Barrena <lucasbarrena98@gmail.com>
2023-01-31 16:43:51 -0300

    Update drive-id.js

--- drive-id.js
+++ drive-id.js
@@ -5,6 +5,7 @@ module.exports = function (id) {
 
   if (id.indexOf('/') > -1 || id.indexOf('.') > -1) return { type: 'path' }
 
+  // + should validate
   if (id.length === 52) return { type: 'key' }
   if (id.length === 64) return { type: 'key' }
 
