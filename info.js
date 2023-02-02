93f7c2195ff82faeecbf3c873aac6d9dba80a724
Lucas Barrena <lucasbarrena98@gmail.com>
2023-02-02 13:44:56 -0300

    info: show size, and content key

--- info.js
+++ info.js
@@ -2,6 +2,8 @@ const Corestore = require('corestore')
 const Hyperdrive = require('hyperdrive')
 const HypercoreId = require('hypercore-id-encoding')
 const fsp = require('fs/promises')
+const byteSize = require('tiny-byte-size')
+const crayon = require('tiny-crayon')
 
 module.exports = async function cmd (key, options = {}) {
   if (options.corestore && typeof options.corestore !== 'string') errorAndExit('--corestore <path> is required as string')
@@ -12,12 +14,19 @@ module.exports = async function cmd (key, options = {}) {
   const store = new Corestore(options.corestore)
   const drive = new Hyperdrive(store, key ? HypercoreId.decode(key) : null)
   await drive.ready()
-  console.log('Loading drive info...')
 
-  console.log('Public key:', HypercoreId.encode(drive.key))
-  console.log('Discovery key:', drive.discoveryKey.toString('hex'))
+  console.log(crayon.gray('Loading drive info...'))
+  console.log()
+
+  console.log('Key:', crayon.magenta(HypercoreId.encode(drive.key)))
+  if (drive.contentKey) console.log('Content key:', crayon.magenta(HypercoreId.encode(drive.contentKey)))
   console.log('Version:', drive.version)
   console.log('Writable?', drive.db.feed.writable)
+  if (!drive.blobs) console.log('Drive is empty')
+  else {
+    const { storage } = await drive.blobs.core.info({ storage: true })
+    console.log('Drive size:', crayon.cyan(byteSize(storage.oplog + storage.tree + storage.blocks + storage.bitfield)))
+  }
 }
 
 async function stat (path) {
