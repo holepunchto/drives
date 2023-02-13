2dd721c85e4433759a072c13cc3dc21a2a01b693
Lucas Barrena <lucasbarrena98@gmail.com>
2023-02-13 18:51:52 -0300

    use serve-drive

--- serve.js
+++ serve.js
@@ -5,11 +5,10 @@ const Hyperswarm = require('hyperswarm')
 const HypercoreId = require('hypercore-id-encoding')
 const driveId = require('./lib/drive-id')
 const http = require('http')
-const rangeParser = require('range-parser')
 const goodbye = require('graceful-goodbye')
 const graceful = require('graceful-http')
 const crayon = require('tiny-crayon')
-const mime = require('mime-types')
+const serve = require('serve-drive')
 
 module.exports = async function cmd (src, options = {}) {
   if (options.corestore && typeof options.corestore !== 'string') errorAndExit('--corestore <path> is required as string')
@@ -46,69 +45,19 @@ module.exports = async function cmd (src, options = {}) {
     swarm.flush().then(done, done)
   }
 
-  const server = http.createServer(async function (req, res) {
-    if (req.method !== 'GET' && req.method !== 'HEAD') {
-      res.writeHead(400).end()
-      return
-    }
+  const server = http.createServer()
+  const close = graceful(server)
 
-    const { pathname } = new URL(req.url, 'http://localhost')
-    const filename = decodeURI(pathname)
-    const entry = await drive.entry(filename)
-
-    if (!entry || !entry.value.blob) {
-      res.writeHead(404).end()
-      return
-    }
-
-    const contentType = mime.lookup(filename)
-    res.setHeader('Content-Type', contentType === false ? 'application/octet-stream' : contentType)
-    res.setHeader('Accept-Ranges', 'bytes')
-
-    let rs
-
-    if (req.headers.range) {
-      const ranges = rangeParser(entry.value.blob.byteLength, req.headers.range)
-
-      if (ranges === -1 || ranges === -2) {
-        res.statusCode = 206
-        res.setHeader('Content-Length', 0)
-        res.end()
-        return
-      }
-
-      const range = ranges[0]
-      const byteLength = range.end - range.start + 1
-
-      res.statusCode = 206
-      res.setHeader('Content-Range', 'bytes ' + range.start + '-' + range.end + '/' + entry.value.blob.byteLength)
-      res.setHeader('Content-Length', byteLength)
-
-      rs = drive.createReadStream(filename, { start: range.start, length: byteLength })
-    } else {
-      res.setHeader('Content-Length', entry.value.blob.byteLength)
-
-      rs = drive.createReadStream(filename, { start: 0, length: entry.value.blob.byteLength })
-    }
-
-    rs.pipe(res, noop)
+  await serve(drive, {
+    port: options.port,
+    host: options.host,
+    anyPort: !options.disableAnyPort,
+    server
   })
 
-  server.listen(options.port, options.host)
-
-  if (!options.disableAnyPort) {
-    server.once('error', function (err) {
-      if (err.code !== 'EADDRINUSE') throw err
-      server.listen(0, options.host)
-    })
-  }
-
-  const close = graceful(server)
   goodbye(() => close(), 1)
 
-  server.once('listening', function () {
-    console.log('HTTP server on http://' + getHost(server.address().address) + ':' + server.address().port)
-  })
+  console.log('HTTP server on http://' + getHost(server.address().address) + ':' + server.address().port)
 }
 
 function getHost (address) {
@@ -135,5 +84,3 @@ function errorAndExit (message) {
   console.error('Error:', message)
   process.exit(1)
 }
-
-function noop () {}
