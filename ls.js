8e50651a7902303418c6e15f8a71bf4a5713ba30
Lucas <lucasbarrena98@gmail.com>
2023-04-13 09:15:26 +0800

    ls swarms the drive (#11)
    * ls swarms the drive
    
    * automatically create corestore for ls
    
    * catch common errors on ctrl-c
    
    * Update ls.js

--- ls.js
+++ ls.js
@@ -1,34 +1,43 @@
-const driveId = require('./lib/drive-id')
+const Hyperswarm = require('hyperswarm')
+const Hyperdrive = require('hyperdrive')
 const goodbye = require('graceful-goodbye')
 const crayon = require('tiny-crayon')
 const byteSize = require('tiny-byte-size')
-const stat = require('./lib/stat.js')
 const errorAndExit = require('./lib/exit.js')
 const getDrive = require('./lib/get-drive.js')
+const swarming = require('./lib/swarming.js')
 
 module.exports = async function cmd (src, options = {}) {
   if (options.prefix && typeof options.prefix !== 'string') errorAndExit('--prefix <path> must be a string')
   if (options.corestore && typeof options.corestore !== 'string') errorAndExit('--corestore <path> must be a string')
   if (!options.corestore) options.corestore = './corestore'
 
-  const id = driveId(src)
-
-  if (id.type === 'key') {
-    if (await stat(options.corestore) === null) errorAndExit('--corestore path does not exists')
-  }
-
   const drive = getDrive(src, options.corestore)
 
   goodbye(() => drive.close())
   await drive.ready()
 
+  if (drive instanceof Hyperdrive) {
+    const swarm = new Hyperswarm()
+    goodbye(() => swarm.destroy(), 1)
+
+    swarming(swarm, drive, options)
+  }
+
   const prefix = options.prefix || '/'
 
-  for await (const name of drive.readdir(prefix)) {
-    const key = require('path').join(prefix, name)
-    const entry = await drive.entry(key)
-    console.log(await formatEntry(drive, entry, name))
+  try {
+    for await (const name of drive.readdir(prefix)) {
+      const key = require('path').join(prefix, name)
+      const entry = await drive.entry(key)
+      console.log(await formatEntry(drive, entry, name))
+    }
+  } catch (err) {
+    // Ignore errors related to CTRL-C: random-access-storage, and Hypercore session
+    if (!(err.message === 'Closed' || err.code === 'SESSION_CLOSED')) throw err
   }
+
+  goodbye.exit()
 }
 
 async function formatEntry (drive, entry, name) {
