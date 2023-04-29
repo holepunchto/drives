36c6e9b024c1b01eb9c1a6229749074a99d819f1
Lucas Barrena <lucasbarrena98@gmail.com>
2023-04-29 20:08:41 -0300

    Fix filter to also ignore sub folders

--- mirror.js
+++ mirror.js
@@ -154,7 +154,7 @@ function generateFilter (custom) {
   if (custom) ignore.push(...custom)
 
   const str = ignore.map(key => key.replace(/[/.\\\s]/g, '\\$&'))
-  const expr = '^\\/(' + str.join('|') + ')(\\/|$)'
+  const expr = '\\/(' + str.join('|') + ')(\\/|$)'
   const regex = new RegExp(expr)
 
   return function filter (key) {
