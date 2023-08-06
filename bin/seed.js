3436c1628921796c6ae5f88857c2447cf5404d6d
Lucas Barrena <lucasbarrena98@gmail.com>
2023-08-06 04:55:48 -0300

    Interactive seed (better UX)

--- bin/seed.js
+++ bin/seed.js
@@ -3,6 +3,7 @@ const Hyperdrive = require('hyperdrive')
 const Hyperswarm = require('hyperswarm')
 const goodbye = require('graceful-goodbye')
 const HypercoreId = require('hypercore-id-encoding')
+const ansiDiff = require('ansi-diff')
 const crayon = require('tiny-crayon')
 const errorAndExit = require('../lib/exit.js')
 const { findCorestore, noticeStorage } = require('../lib/find-corestore.js')
@@ -12,6 +13,7 @@ module.exports = async function cmd (key, options = {}) {
 
   const storage = await findCorestore(options)
   await noticeStorage(storage)
+  console.log()
 
   const swarm = new Hyperswarm()
   const store = new Corestore(storage)
@@ -22,24 +24,113 @@ module.exports = async function cmd (key, options = {}) {
 
   await drive.ready()
 
-  console.log('Key:', crayon.magenta(HypercoreId.encode(drive.key)))
-  console.log()
+  const diff = ansiDiff()
+  const state = {
+    announced: false
+  }
 
-  console.log('Seeding drive...')
-
-  swarm.on('connection', onsocket)
+  swarm.on('connection', (socket) => drive.replicate(socket))
   const discovery = swarm.join(drive.discoveryKey)
 
-  await discovery.flushed()
-  console.log(crayon.cyan('(Swarm)'), 'Drive is being shared')
+  discovery.flushed().then(() => {
+    state.announced = true
+    onpeertick(diff, drive, state)
+  })
 
-  function onsocket (socket) {
-    const remoteInfo = socket.rawStream.remoteHost + ':' + socket.rawStream.remotePort
-    const pk = HypercoreId.encode(socket.remotePublicKey)
+  oncore(diff, drive, drive.core, state)
 
-    console.log(crayon.cyan('(Swarm)'), 'Peer opened (' + swarm.connections.size + ')', crayon.gray(remoteInfo), crayon.magenta(pk))
-    socket.on('close', () => console.log(crayon.cyan('(Swarm)'), 'Peer closed (' + swarm.connections.size + ')', crayon.gray(remoteInfo), crayon.magenta(pk)))
+  if (drive.blobs) oncore(diff, drive, drive.blobs.core, state)
+  else drive.once('blobs', blobs => oncore(diff, drive, blobs.core, state))
 
-    drive.corestore.replicate(socket)
+  // TODO: We need a 'peer-update' event in Hypercore
+  const intervalId = setInterval(() => onpeertick(diff, drive, state), 1000)
+  goodbye(() => clearInterval(intervalId))
+}
+
+function oncore (diff, drive, core, state) {
+  core.on('peer-add', onpeertick.bind(null, diff, drive, state))
+  core.on('peer-remove', onpeertick.bind(null, diff, drive, state))
+  onpeertick(diff, drive, state)
+}
+
+function onpeertick (diff, drive, state = {}) {
+  const peers = new Map() // Recreating it every time simplifies things
+
+  for (const peer of drive.core.peers) onpeer(peer, 'core')
+  for (const peer of drive.blobs?.core.peers || []) onpeer(peer, 'blobs')
+
+  update(diff, render)
+
+  function onpeer (peer, type) {
+    const id = HypercoreId.encode(peer.remotePublicKey)
+
+    const existing = peers.get(id)
+    const info = existing || {
+      core: { peer: null },
+      blobs: { peer: null }
+    }
+
+    if (type === 'core') info.core.peer = peer
+    else info.blobs.peer = peer
+
+    if (!existing) peers.set(id, info)
   }
+
+  function render () {
+    return [
+      `
+        ${state.announced ? crayon.cyan('Announced') : crayon.red('Announcing...')}
+        Files: ${crayon.green(drive.id)}
+        ${drive.contentKey ? `Blobs: ${crayon.green(HypercoreId.encode(drive.contentKey))}` : ''}
+      `,
+
+      onpeers()
+    ]
+  }
+
+  function onpeers () {
+    if (peers.size === 0) return crayon.red('No peers')
+
+    const output = ['Peers']
+    let count = 0
+
+    for (const [id, info] of peers) {
+      const key = id.slice(0, 5) + '..' + id.slice(-5)
+      const remoteInfo = info.core.peer?.stream.rawStream.remoteHost
+
+      output.push(
+        '- ' +
+        (remoteInfo ? crayon.gray(remoteInfo) + ' ' : '') +
+        crayon.magenta(key) + ' ' +
+        crayon.yellow((info.core.peer?.remoteContiguousLength || 0) + '/' + drive.core.length) + ' + ' +
+        crayon.yellow((info.blobs.peer?.remoteContiguousLength || 0) + '/' + (drive.blobs?.core.length || 0)) + ' blks'
+      )
+
+      if (++count === 5) break
+    }
+
+    if (peers.size - count > 0) output.push('... and ' + crayon.yellow(peers.size - count) + ' more connections')
+
+    return output.join('\n')
+  }
+}
+
+// TODO: This could end up being a small lib for a simple rendering solution?
+function update (diff, render) {
+  const original = render()
+  const blocks = Array.isArray(original) ? original : [original]
+  const processed = []
+
+  for (let block of blocks) {
+    if (block === null) continue
+
+    block = block.replace(/  +/g, ' ') // Remove double spaces
+    // block = block.replace(/(\s\s)+/g, '$1') // Remove double spaces
+    block = block.replace(/^ +/gm, '') // Remove identation
+    block = block.trim()
+
+    processed.push(block)
+  }
+
+  process.stdout.write(diff.update(processed.join('\n\n') + '\n'))
 }
